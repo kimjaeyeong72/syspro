63c4ddb8d1a10d3241bd060d312bc0b5fad8822f
kimjaeyeong72 <kmdkjy5896@gmail.com>
2023-10-24 05:29:47 -0700

    daskjasfkajklsdk

--- chap4/prob10/cat.c
+++ chap4/prob10/cat.c
@@ -1,20 +1,57 @@
 #include <stdio.h>
-int main(int argc, char *argv[])
-{
-   FILE *fp;
-   int c;
-   int i;
-for (i =1; i <argc; i++) {
-   if (argc < 2)
-      fp = stdin;
-   else  fp = fopen(argv[1],"r");
+#include <stdlib.h>
+#include <string.h>
 
-   c = getc(fp);
-   while (c!=EOF) {
-	   putc(c,stdout);
-	   c = getc(fp);
-   }
-   fclose(fp);
+void copyFile(FILE *input, FILE *output, int addLineNumbers) {
+    int c;
+    int lineNumber = 1;
+    int newLine = 1;
+
+    while ((c = getc(input)) != EOF) {
+        if (addLineNumbers && newLine) {
+            fprintf(output, "%d ", lineNumber);
+            newLine = 0;
+        }
+        fputc(c, output);
+
+        if (c == '\n') {
+            lineNumber++;
+            newLine = 1;
+        }
+    }
+
+    if (!newLine) {
+        fputc('\n', output);
+    }
 }
-   return 0;
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr,"test\n",argv[0]);
+	fprintf(stderr,"test\n",argv[0]);
+        return 1;
+    }
+
+    int addLineNumbers = 0;
+    int fileIndex = 1;
+
+    if (argc > 2 && strcmp(argv[1], "-n") == 0) {
+        addLineNumbers = 1;
+        fileIndex = 2;
+    }
+
+    for (int i = fileIndex; i < argc; i++) {
+        FILE *input = fopen(argv[i], "r");
+        if (input == NULL) {
+            perror("Error opening file");
+            return 1;
+        }
+
+        copyFile(input, stdout, addLineNumbers);
+
+        fclose(input);
+    }
+
+    return 0;
 }
+
