dfe4ecdda6019ed2265384785a603afdcbccecbf
kimjaeyeong <kmdkjy5896@gmail.com>
2023-10-25 06:34:26 +0000

    kklas

--- midterm/2/bookcreate.c
+++ midterm/2/bookcreate.c
@@ -1,18 +1,20 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[])
-{
-   struct rec;
-   FILE *fp;
+ {
+   struct book  rec;
+  FILE *fp;
    if (argc != 2) {
-      fprintf(stderr, “How to use: %s FileName\n”, argv[0]);
-      return 1;
-   }
-   fp = fopen(argv[1], "w");
-   printf("%-9s %-7s %-4d %-5s %-5s %-5d\n", “ID", “bookName", “author", "year","numofborrow","borrow);
-   while (scanf("%s %s %d %s %s %d", &rec.ID, rec.bookName, &rec.author, &rec.year, &rec.numofborrow,&rec.borrow)==6)
-      fprintf(fp, "%s %s %d %s %s %d", &rec.ID, rec.bookName, &rec.author, &rec.year, &rec.numofborrow,&rec.borrow);
-   fclose(fp);
-   return 0;
-}
-
+       fprintf(stderr, "How to use: %s FileName\n", argv[0]);
+        exit(1);
+     }
+     fp = fopen(argv[1], "wb");
+     printf("%-9s %-7s %-4s %-5s %-5s %-5s\n", "ID", "bookName", "author", "year","numofborrow","borrow");
+     while (scanf("%d %s %d %s %s %d", &rec.ID, &rec.bookName, &rec.author, &rec.year, &rec.numofborrow,&rec.borrow)==6)
+	 fwrite(&rec, sizeof(rec), 1, fp);
+     fclose(fp);
+     return 0;
+  }
+ 
