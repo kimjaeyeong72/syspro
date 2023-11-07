6779ef0701839d5d0f2cc9fe44724823723a629a
kimjaeyeong72 <kmdkjy5896@gmail.com>
2023-11-07 06:35:18 -0800

    tlftmq

--- chap6/prob3/main.c
+++ chap6/prob3/main.c
@@ -1,21 +1,21 @@
-#include <sys/types.h>
-#include <utime.h>
-#include <sys/stat.h>
-#include <stdio.h>
+#include <sys/types.h> 
+#include <sys/stat.h> 
 #include <sys/time.h>
+#include <utime.h>       
+#include <stdio.h>      
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) 
 {
-    struct stat buf;        
+    struct stat buf;     
     struct utimbuf time;
 
     if (argc < 3) {
-        fprintf(stderr, “How to use: cptime file1 file2\n");
+        fprintf(stderr,"How to use : cptime file1 file2\n");
         exit(1);
     }
-
-    if (stat(argv[1], &buf) <0) { 
+ 
+    if (stat(argv[1], &buf) <0) {
         perror("stat()");
         exit(-1);
     }
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
     time.actime = buf.st_atime;
     time.modtime = buf.st_mtime;
 
-    if (utime(argv[2], &time))   
+    if (utime(argv[2], &time)) 
         perror("utime");
     else exit(0);
 }
